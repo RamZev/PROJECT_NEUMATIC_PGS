4467f4bc086f276c62f655c644afef3edb0308bb
Ricardo Ramos <ramosric1410@gmail.com>
2026-04-30 18:54:32 -0500

    Segundo commit del Proyecto

--- neumatic/data_load/actualiza_cliente.sql
+++ neumatic/data_load/actualiza_cliente.sql
@@ -1,2 +1,2 @@
-UPDATE cliente SET cliente_empresa = 1 WHERE id_cliente = 30007;
+UPDATE cliente SET cliente_empresa = true WHERE id_cliente = 30007;
 DELETE FROM cliente WHERE id_cliente = 0;
--- neumatic/data_load/forma_pago.sql
+++ neumatic/data_load/forma_pago.sql
@@ -1,12 +1,33 @@
 BEGIN TRANSACTION;
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:00:26',1,1,'EFECTIVO',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:00:36',2,1,'CUENTA CORRIENTE',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:00:44',3,1,'TARJETA CREDITO',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:00:53',4,1,'TARJETA DEBITO',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:01:03',5,1,'DEPOSITO',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:01:26',6,1,'DOLARES',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:01:35',7,1,'CHEQUES',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:01:44',8,1,'RETENCIONES',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:01:55',9,1,'COMPENSA. FACTURAS',1);
-INSERT INTO "forma_pago" VALUES ('admin','ESTACION03','2025-11-25 22:02:05',10,1,'MERCADO PAGO',1);
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:00:26', 1, TRUE, 'EFECTIVO');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:00:36', 2, TRUE, 'CUENTA CORRIENTE');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:00:44', 3, TRUE, 'TARJETA CREDITO');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:00:53', 4, TRUE, 'TARJETA DEBITO');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:01:03', 5, TRUE, 'DEPOSITO');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:01:26', 6, TRUE, 'DOLARES');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:01:35', 7, TRUE, 'CHEQUES');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:01:44', 8, TRUE, 'RETENCIONES');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:01:55', 9, TRUE, 'COMPENSA. FACTURAS');
+
+INSERT INTO forma_pago (usuario, estacion, fcontrol, id_forma_pago, estatus_forma_pago, descripcion_forma_pago) 
+VALUES ('admin', 'ESTACION03', '2025-11-25 22:02:05', 10, TRUE, 'MERCADO PAGO');
+
 COMMIT;
