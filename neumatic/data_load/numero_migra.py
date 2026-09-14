abf42fcb9ea7b9e573f8b249a83dc74b33a04af1
Ricardo Ramos <ramosric1410@gmail.com>
2026-09-14 13:14:44 -0500

    1409-2

--- neumatic/data_load/numero_migra.py
+++ neumatic/data_load/numero_migra.py
@@ -45,7 +45,8 @@ def reset_numero():
 
 def cargar_datos():
     """Lee los datos del archivo Excel, migra los datos al modelo Numero."""
-    reset_numero()  # Eliminar datos existentes antes de migrar
+    # Deshabilitado temporalmente ojo ojo
+    # reset_numero()  # Eliminar datos existentes antes de migrar
 
     # Ruta del archivo Excel
     excel_path = os.path.join(BASE_DIR, 'data_load', 'numeracion.xlsx')
